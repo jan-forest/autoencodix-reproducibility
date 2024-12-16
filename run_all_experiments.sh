7a63789843d94f98529cb0100669a55e8be14fb8
Maximilian Joas <maximilianjoas@scadsnb49.misc.intern.uni-leipzig.de>
2024-12-16 08:16:49 +0100

    untoggled code

--- run_all_experiments.sh
+++ run_all_experiments.sh
@@ -75,132 +75,132 @@ rm ./Exp1_*_Annealing_config.yaml
 log_message "Experiment 1 done"
 ###################################
 
-# #### Exp2 AE comparison ###########
-# log_message "Starting Experiment 2: AE comparison"
-# # copy cfg in root
-# cp ./config_runs/Exp2/*_config.yaml .
+#### Exp2 AE comparison ###########
+log_message "Starting Experiment 2: AE comparison"
+# copy cfg in root
+cp ./config_runs/Exp2/*_config.yaml .
 
-# # run AUTOENCODIX
-# for config in ./Exp2*_config.yaml; do
-#     log_message "Current run: $(basename $config _config.yaml)"
-#     make ml_task RUN_ID=$(basename $config _config.yaml)
-#     bash ./clean.sh -r $(basename $config _config.yaml) -d -k # Clean up and keep only reports folder
-# done
+# run AUTOENCODIX
+for config in ./Exp2*_config.yaml; do
+    log_message "Current run: $(basename $config _config.yaml)"
+    make ml_task RUN_ID=$(basename $config _config.yaml)
+    bash ./clean.sh -r $(basename $config _config.yaml) -d -k # Clean up and keep only reports folder
+done
 
-# # get paper visualization
-# mkdir -p ./reports/paper-visualizations/Exp2
-# $PYTHON_INTERPRETER ./src/visualization/Exp2_visualization.py
+# get paper visualization
+mkdir -p ./reports/paper-visualizations/Exp2
+$PYTHON_INTERPRETER ./src/visualization/Exp2_visualization.py
 
-# log_message "Cleaning up Experiment 2"
-# # clean up
-# rm ./Exp2*_config.yaml
-# log_message "Experiment 2 done"
-# ###################################
+log_message "Cleaning up Experiment 2"
+# clean up
+rm ./Exp2*_config.yaml
+log_message "Experiment 2 done"
+###################################
 
-# #### Exp3 Ontix robustness ########
-# log_message "Starting Experiment 3: ontix robustness"
-# # copy cfg in root
-# cp ./config_runs/Exp3/*_config.yaml .
+#### Exp3 Ontix robustness ########
+log_message "Starting Experiment 3: ontix robustness"
+# copy cfg in root
+cp ./config_runs/Exp3/*_config.yaml .
 
-# # run AUTOENCODIX
-# for config in ./Exp3*_config.yaml; do
-#     log_message "Current run: $(basename $config _config.yaml)"
-#     make ml_task RUN_ID=$(basename $config _config.yaml)       # make visualize might be sufficient
-#     bash ./clean.sh -r $(basename $config _config.yaml) -k -d # Clean up and keep only reports folder
-# done
+# run AUTOENCODIX
+for config in ./Exp3*_config.yaml; do
+    log_message "Current run: $(basename $config _config.yaml)"
+    make ml_task RUN_ID=$(basename $config _config.yaml)       # make visualize might be sufficient
+    bash ./clean.sh -r $(basename $config _config.yaml) -k -d # Clean up and keep only reports folder
+done
 
-# # get paper visualization
-# mkdir -p ./reports/paper-visualizations/Exp3
-# $PYTHON_INTERPRETER ./src/visualization/Exp3_visualization.py
+# get paper visualization
+mkdir -p ./reports/paper-visualizations/Exp3
+$PYTHON_INTERPRETER ./src/visualization/Exp3_visualization.py
 
-# log_message "Cleaning up Experiment 3"
-# # clean up
-# rm ./Exp3*_config.yaml
-# log_message "Experiment 3 done"
+log_message "Cleaning up Experiment 3"
+# clean up
+rm ./Exp3*_config.yaml
+log_message "Experiment 3 done"
 
-# #### Exp4 Celegans and TF Expression ####
-# log_message "Starting Experiment 4: Celegans and TF Expression"
-# mkdir -p ./reports/paper-visualizations/Exp4
-# cp ./config_runs/Exp4/*.yaml .
+#### Exp4 Celegans and TF Expression ####
+log_message "Starting Experiment 4: Celegans and TF Expression"
+mkdir -p ./reports/paper-visualizations/Exp4
+cp ./config_runs/Exp4/*.yaml .
 
-# make visualize RUN_ID=Exp4_Celegans_TF
-# log_message "Experiment 4 main run done"
+make visualize RUN_ID=Exp4_Celegans_TF
+log_message "Experiment 4 main run done"
 
-# log_message "Starting Img_to_Img run for comparison"
-# make visualize RUN_ID=Exp4_Celegans_TFImgImg
-# log_message "Experiment 4 Img_to_Img run done"
+log_message "Starting Img_to_Img run for comparison"
+make visualize RUN_ID=Exp4_Celegans_TFImgImg
+log_message "Experiment 4 Img_to_Img run done"
 
-# log_message "Starting Celegans extra visualization"
-# log_message "Creating GIFs"
-# $PYTHON_INTERPRETER eval-xmodalix-scripts/generate_gif.py Exp4_Celegans_TF # Corrected to use $PYTHON_INTERPRETER
-# log_message "Creating GIFs done"
+log_message "Starting Celegans extra visualization"
+log_message "Creating GIFs"
+$PYTHON_INTERPRETER eval-xmodalix-scripts/generate_gif.py Exp4_Celegans_TF # Corrected to use $PYTHON_INTERPRETER
+log_message "Creating GIFs done"
 
-# log_message "Creating comparison plots"
-# $PYTHON_INTERPRETER eval-xmodalix-scripts/plot_compare.py Exp4_Celegans_TF # Corrected to use $PYTHON_INTERPRETER
-# log_message "Creating comparison plots done"
-# log_message "Eval against Img_to_Img"
-# $PYTHON_INTERPRETER eval-xmodalix-scripts/eval_against_ImgImg.py Exp4_Celegans_TF # Corrected to use $PYTHON_INTERPRETER
-# log_message "Eval against Img_to_Img done"
+log_message "Creating comparison plots"
+$PYTHON_INTERPRETER eval-xmodalix-scripts/plot_compare.py Exp4_Celegans_TF # Corrected to use $PYTHON_INTERPRETER
+log_message "Creating comparison plots done"
+log_message "Eval against Img_to_Img"
+$PYTHON_INTERPRETER eval-xmodalix-scripts/eval_against_ImgImg.py Exp4_Celegans_TF # Corrected to use $PYTHON_INTERPRETER
+log_message "Eval against Img_to_Img done"
 
-# log_message "Eval xmodalix with classification"
-# $PYTHON_INTERPRETER eval-xmodalix-scripts/eval_xmodalix.py Exp4_Celegans_TF # Corrected to use $PYTHON_INTERPRETER
-# log_message "Eval xmodalix with classification done"
+log_message "Eval xmodalix with classification"
+$PYTHON_INTERPRETER eval-xmodalix-scripts/eval_xmodalix.py Exp4_Celegans_TF # Corrected to use $PYTHON_INTERPRETER
+log_message "Eval xmodalix with classification done"
 
-# log_message "Exp4 X-Modalix with regression"
-# $PYTHON_INTERPRETER eval-xmodalix-scripts/eval_xmodalix_regression.py Exp4_Celegans_TF # Corrected to use $PYTHON_INTERPRETER
-# log_message "Exp4 X-Modalix with regression done"
+log_message "Exp4 X-Modalix with regression"
+$PYTHON_INTERPRETER eval-xmodalix-scripts/eval_xmodalix_regression.py Exp4_Celegans_TF # Corrected to use $PYTHON_INTERPRETER
+log_message "Exp4 X-Modalix with regression done"
 
-# # Get paper visualization
-# log_message "Copying visualizations to reports/paper-visualizations/Exp4"
-# cp ./reports/Exp4_Celegans_TF/figures/* ./reports/paper-visualizations/Exp4
-# cp ./reports/Exp4_Celegans_TF/*.csv ./reports/paper-visualizations/Exp4 
+# Get paper visualization
+log_message "Copying visualizations to reports/paper-visualizations/Exp4"
+cp ./reports/Exp4_Celegans_TF/figures/* ./reports/paper-visualizations/Exp4
+cp ./reports/Exp4_Celegans_TF/*.csv ./reports/paper-visualizations/Exp4 
 
-# # clean up
-# bash ./clean.sh -r Exp4_Celegans_TF,Exp4_CelegansImgImg -k -d # Clean up and keep only reports folder
-# rm ./Exp4_Celegans_TF_config.yaml
-# rm ./Exp4_Celegans_TFImgImg_config.yaml
-# log_message "Exp4 removed intermediate data"
+# clean up
+bash ./clean.sh -r Exp4_Celegans_TF,Exp4_CelegansImgImg -k -d # Clean up and keep only reports folder
+rm ./Exp4_Celegans_TF_config.yaml
+rm ./Exp4_Celegans_TFImgImg_config.yaml
+log_message "Exp4 removed intermediate data"
 
-# log_message "Exp4 ALL DONE"
+log_message "Exp4 ALL DONE"
 
 
-# #### Exp5 TCGA and MNIST ####
-# log_message "Starting Experiment 5: TCGA and MNIST"
-# mkdir -p ./reports/paper-visualizations/Exp5
-# cp ./config_runs/Exp5/*.yaml .
-# make visualize RUN_ID=Exp5_TCGA_MNIST
-# log_message "Experiment 5 main run done"
+#### Exp5 TCGA and MNIST ####
+log_message "Starting Experiment 5: TCGA and MNIST"
+mkdir -p ./reports/paper-visualizations/Exp5
+cp ./config_runs/Exp5/*.yaml .
+make visualize RUN_ID=Exp5_TCGA_MNIST
+log_message "Experiment 5 main run done"
 
-# log_message "Starting Img_to_Img run for comparison"
-# make visualize RUN_ID=Exp5_TCGA_MNISTImgImg
-# log_message "Experiment 5 Img_to_Img run done"
+log_message "Starting Img_to_Img run for comparison"
+make visualize RUN_ID=Exp5_TCGA_MNISTImgImg
+log_message "Experiment 5 Img_to_Img run done"
 
-# log_message "Starting TCGA extra visualization"
+log_message "Starting TCGA extra visualization"
 
-# log_message "Creating comparison plots"
-# $PYTHON_INTERPRETER eval-xmodalix-scripts/plot_compare_tcga.py Exp5_TCGA_MNIST # Corrected to use $PYTHON_INTERPRETER
-# log_message "Creating comparison plots done"
+log_message "Creating comparison plots"
+$PYTHON_INTERPRETER eval-xmodalix-scripts/plot_compare_tcga.py Exp5_TCGA_MNIST # Corrected to use $PYTHON_INTERPRETER
+log_message "Creating comparison plots done"
 
-# log_message "Eval against Img_to_Img"
-# $PYTHON_INTERPRETER eval-xmodalix-scripts/eval_against_ImgImg.py Exp5_TCGA_MNIST # Corrected to use $PYTHON_INTERPRETER
-# log_message "Eval against Img_to_Img done"
+log_message "Eval against Img_to_Img"
+$PYTHON_INTERPRETER eval-xmodalix-scripts/eval_against_ImgImg.py Exp5_TCGA_MNIST # Corrected to use $PYTHON_INTERPRETER
+log_message "Eval against Img_to_Img done"
 
-# log_message "Eval xmodalix with classification"
-# $PYTHON_INTERPRETER eval-xmodalix-scripts/eval_xmodalix.py Exp5_TCGA_MNIST # Corrected to use $PYTHON_INTERPRETER
-# log_message "Eval xmodalix with classification done"
+log_message "Eval xmodalix with classification"
+$PYTHON_INTERPRETER eval-xmodalix-scripts/eval_xmodalix.py Exp5_TCGA_MNIST # Corrected to use $PYTHON_INTERPRETER
+log_message "Eval xmodalix with classification done"
 
-# #### SKIPPING THIS PART FOR TCGA BECAUSE WE HAVE NO TIMESEREIS DATA
-# og_message "Creating GIFs"
-# $PYTHON_INTERPRETER eval-xmodalix-scripts/generate_gif.py Exp5_TCGA_MNIST # Corrected to use $PYTHON_INTERPRETER
-# log_message "Creating GIFs done"
-# # clean up
-# bash ./clean.sh -r Exp5_TCGA_MNIST,Exp5_TCGA_MNISTImggImg -k -d # Clean up and keep only reports folder
-# rm ./Exp5_Exp5_TCGA_MNIST_config.yaml
-# rm ./Exp5_Exp5_TCGA_MNISTImgImg_config.yaml
-# log_message "Exp5 removed intermediate data"
+#### SKIPPING THIS PART FOR TCGA BECAUSE WE HAVE NO TIMESEREIS DATA
+og_message "Creating GIFs"
+$PYTHON_INTERPRETER eval-xmodalix-scripts/generate_gif.py Exp5_TCGA_MNIST # Corrected to use $PYTHON_INTERPRETER
+log_message "Creating GIFs done"
+# clean up
+bash ./clean.sh -r Exp5_TCGA_MNIST,Exp5_TCGA_MNISTImggImg -k -d # Clean up and keep only reports folder
+rm ./Exp5_Exp5_TCGA_MNIST_config.yaml
+rm ./Exp5_Exp5_TCGA_MNISTImgImg_config.yaml
+log_message "Exp5 removed intermediate data"
 
-# log_message "Exp5 ALL DONE"
-# # Get paper visualization
-# log_message "Copying visualizations to reports/paper-visualizations/Exp4"
-# cp ./reports/Exp5_TCGA_MNIST/figures/* ./reports/paper-visualizations/Exp5
-# cp ./reports/Exp5_TCGA_MNIST/*.csv ./reports/paper-visualizations/Exp5
+log_message "Exp5 ALL DONE"
+# Get paper visualization
+log_message "Copying visualizations to reports/paper-visualizations/Exp4"
+cp ./reports/Exp5_TCGA_MNIST/figures/* ./reports/paper-visualizations/Exp5
+cp ./reports/Exp5_TCGA_MNIST/*.csv ./reports/paper-visualizations/Exp5
