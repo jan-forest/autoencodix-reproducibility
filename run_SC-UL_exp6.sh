50532110d4a0a52e5a37dadde1bb7515482d7d8e
Maximilian Joas <maximilianjoas@scadsnb49.misc.intern.uni-leipzig.de>
2025-05-07 09:40:06 +0200

    added SC file for exp6

---
+++ run_SC-UL_exp6.sh
@@ -0,0 +1,31 @@
+sbatch <<EOT
+#!/bin/bash
+#SBATCH --job-name=autoencoder_Exp6_TCGA_METH_RNA
+#SBATCH --output=./reports/Exp6_TCGA_METH_RNA/slurm_%a_%j.out
+#SBATCH --error=./reports/Exp6_TCGA_METH_RNA/slurm_%a_%j.err
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --partition=clara
+#SBATCH --time=4:00:00
+#SBATCH --cpus-per-task=8
+#SBATCH --gres=gpu:1
+#SBATCH --mem=64GB
+#SBATCH --gres=gpu:rtx2080ti:1
+
+echo "Doing Experiment 6: TCGA_METH_RNA X-Modalix"
+ml cuDNN
+export __MODIN_AUTOIMPORT_PANDAS_=:1
+
+./run_exp5.sh
+if [ -z "$VIRTUAL_ENV" ];
+then 
+	source venv-gallia/bin/activate
+	echo $VIRTUAL_ENV
+fi
+nephelai upload-with-fs reports/Exp6_TCGA_METH_RNA
+nephelai upload-with-fs reports/Exp6_TCGA_METH_RNA
+nephelai upload-with-fs reports/paper-visualizations/Exp6
+# bash ./clean.sh -r Exp6_TCGA_METH_RNA,Exp6_TCGA_METH_METH -k -d # Clean up and keep only reports folder
+
+exit 0
+EOT
